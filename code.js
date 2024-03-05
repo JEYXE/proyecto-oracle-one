743f2709adcec4c35aae2212a960d6d01e7d2208
Jorge Caldas <jorge.caldas@outlook.com>
2024-03-04 20:40:38 -0500

    final

--- code.js
+++ code.js
@@ -1,5 +1,5 @@
 function codificar() {
-    let mensaje = document.getElementById('mensaje').value;
+    let mensaje = document.getElementById('mensaje_input').value;
     let tamaño_mensaje= mensaje.length;
     let mensaje_codificado = "";
     for (let i = 0; i < tamaño_mensaje; i++) {
@@ -17,31 +17,31 @@ function codificar() {
             mensaje_codificado += mensaje[i] 
         }   
       }
-      document.getElementById("resultado").value = mensaje_codificado;
-      document.getElementById("mensaje").value = "";
+      document.getElementById("mensaje_output").value = mensaje_codificado;
+      document.getElementById("mensaje_input").value = "";
       document.querySelector('#copiar').disabled = false;
       activacion();
     return ;
 }
 
 function decodificar(){
-    let mensaje_decodificado = document.getElementById('mensaje').value;
+    let mensaje_decodificado = document.getElementById('mensaje_input').value;
     mensaje_decodificado=mensaje_decodificado.replaceAll("ai","a");
     mensaje_decodificado=mensaje_decodificado.replaceAll("enter","e");
     mensaje_decodificado=mensaje_decodificado.replaceAll("imes","i");
     mensaje_decodificado=mensaje_decodificado.replaceAll("ober","o");
     mensaje_decodificado=mensaje_decodificado.replaceAll("ufat","u");
-    document.getElementById("resultado").value =mensaje_decodificado;
-    document.getElementById("mensaje").value = "";
+    document.getElementById("mensaje_output").value =mensaje_decodificado;
+    document.getElementById("mensaje_input").value = "";
     document.querySelector('#copiar').disabled = false;
     activacion();
     return;
 }
 
-document.getElementById('mensaje').addEventListener('input', activacion);
+document.getElementById('mensaje_input').addEventListener('input', activacion);
 
 function activacion()  {
-    if (document.querySelector('#mensaje').value === '') {
+    if (document.querySelector('#mensaje_input').value === '') {
         document.querySelector('#codificar').disabled = true;
         document.querySelector('#decodificar').disabled = true;
       } else {
@@ -54,19 +54,14 @@ function activacion()  {
 
 async function copiarAlPortapapeles() {
     try {
-        await navigator.clipboard.writeText('Este es el texto a copiar');
+        await navigator.clipboard.writeText(document.querySelector('#mensaje_output').value);
         console.log('Contenido copiado al portapapeles');
         /* Resuelto - texto copiado al portapapeles con éxito */
       } catch (err) {
         console.error('Error al copiar: ', err);
         /* Rechazado - fallo al copiar el texto al portapapeles */
       }
-      navigator.permissions.query({ name: "write-on-clipboard" })
-        .then((resultado) => {
-            if (resultado.state == "granted" || resultado.state == "prompt") {
-                alert("¡Permiso de escritura concedido!");
-            }
-            
-        });
+    document.getElementById("mensaje_output").value = "";
+    document.querySelector('#copiar').disabled = true;
 }
 
